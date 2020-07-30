b25565bcffcb4bacd7c2dff3be44a56fed610f51
leegise-lab <55785075+leegise-lab@users.noreply.github.com>
2020-07-30 14:25:28 +0900

    개인프로젝트

---
+++ app/src/main/java/com/example/tripwithyou/Chatting.java
@@ -0,0 +1,19 @@
+package com.example.tripwithyou;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Chatting extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_view);
+
+    }
+}
